ad57f6f7f8764c7d9be743be522502bb489cebf1
Ethan M <ethanmilbert@gmail.com>
2022-04-10 15:53:48 +0000

    commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # zsh-EXTREME
+##### **Note: Python3 is required, if you have not already please install it on your system.**
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #! /bin/zsh
-chmod +x bin/*
+chmod +x src/*
 echo 'PATH="$PATH:'$(dirname $(realpath $0))'/bin"' >> ~/.zshrc
+pip install -r requirements.txt --upgrade
