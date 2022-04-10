509703d16f6f9d9296f941d88b64548bb024d899
drgnfireyellow <86269995+DrgnFireYellow@users.noreply.github.com>
2022-04-10 16:26:29 -0400

    Add functionality to add to rprompt.

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #! /bin/zsh
-chmod +x src/*
+echo "Installing..."
+chmod -R +x "$(dirname $(realpath $0))/src"
 echo 'PATH="$PATH:'$(dirname $(realpath $0))'/src"' >> ~/.zshrc
-pip install -r requirements.txt --upgrade
+python3 -m pip install -q -r requirements.txt
+echo "IMPORTANT: DO NOT DELETE THIS DIRECTORY"
+mkdir "$(dirname $(realpath $0))/data"
+touch "$(dirname $(realpath $0))/data/widgets.sh"
+echo source $(dirname $(realpath $0))/data/widgets.sh >> ~/.zshrc
+echo "Installation Complete!"
