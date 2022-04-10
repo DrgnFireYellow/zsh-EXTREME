9d939fc6e489faaf6bd35f323d72fc290248cf7c
drgnfireyellow <ethan@milbert.dev>
2022-04-10 12:19:15 -0400

    Fix PATH variable

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! /bin/zsh
 chmod +x src/*
-echo 'PATH="$PATH:'$(dirname $(realpath $0))'/bin"' >> ~/.zshrc
+echo 'PATH="$PATH:'$(dirname $(realpath $0))'/src"' >> ~/.zshrc
 pip install -r requirements.txt --upgrade
